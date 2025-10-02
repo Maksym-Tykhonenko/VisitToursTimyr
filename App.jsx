d261a4f382932ee7605fbafc236c70a946329814
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-10-02 11:40:06 +0300

    save

--- App.jsx
+++ App.jsx
@@ -586,7 +586,7 @@ const App = () => {
     //const checkUrl = `https://reactnative.dev/`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-10-29T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-10-07T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (!route) {
